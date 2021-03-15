10ea976365f47c8084ca67f2de79a81f7f58185f
Fabiano Luiz <fabziul@gmail.com>
2021-03-15 18:01:41 -0300

    Projeto Spring Boot criado

---
+++ src/test/java/com/fabziul/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabziul.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
